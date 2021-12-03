2041efae13a24b823c84cee51c208262a5811e25
Nazif Enes Kızılcin <enes.kizilcin@trhqmac215.local>
2021-12-03 09:59:57 +0300

    acceptance test for InputEchoer has made

--- InputEchoer/specs/example.spec
+++ InputEchoer/specs/example.spec
@@ -1,53 +1,14 @@
-# Getting Started with Gauge
+# Input Echoer Acceptance Test
 
-This is an example markdown specification file.
-Every heading in this file is a scenario.
-Every bulleted point is a step.
-
-To execute this specification, use
-	npm test
 
 This is a context step that runs before every scenario
-* Open todo application
 
-## Display number of items
-* Add task "first task"
-* Must display "1 item left"
-* Add task "second task"
-* Must display "2 items left"
+* Open InputEchoer application
 
-## Must list only active tasks
-* Add tasks 
+## Add Echo Item
+* Add text to echo "Hello World"
+* Must display "1 echo item"
 
-   |description|
-   |-----------|
-   |first task |
-   |second task|
-   |third task |
-   |fourth task|
-   |fifth task |
-
-* Complete tasks 
-
-   |description|
-   |-----------|
-   |second task|
-   |fifth task |
-* View "Active" tasks
-* Must have 
-
-   |description|
-   |-----------|
-   |first task |
-   |third task |
-   |fourth task|
-* Must not have 
-
-   |description|
-   |-----------|
-   |second task|
-   |fifth task |
-
-A tear down step for every scenario
-___
-* Clear all tasks
+## Clear Echo Items
+* Clear echo items
+* Must display "0 echo item"
--- InputEchoer/tests/step_implementation.js
+++ InputEchoer/tests/step_implementation.js
@@ -17,7 +17,8 @@ const {
     text,
     into,
     textBox,
-    evaluate
+    evaluate,
+    button
 } = require('taiko');
 const assert = require("assert");
 const headless = process.env.headless_chrome.toLowerCase() === 'true';
@@ -32,59 +33,21 @@ afterSuite(async () => {
     await closeBrowser();
 });
 
-// Return a screenshot file name
-gauge.customScreenshotWriter = async function () {
-    const screenshotFilePath = path.join(process.env['gauge_screenshots_dir'],
-        `screenshot-${process.hrtime.bigint()}.png`);
-
-    await screenshot({
-        path: screenshotFilePath
-    });
-    return path.basename(screenshotFilePath);
-};
-
-step("Add task <item>", async (item) => {
-    await write(item, into(textBox("What needs to be done?")));
-    await press('Enter');
+step("Open InputEchoer application", async function () {
+    await goto("localhost:3000/");
 });
 
-step("View <type> tasks", async function (type) {
-    await click(link(type));
-});
-
-step("Complete tasks <table>", async function (table) {
-    for (var row of table.rows) {
-        await click(checkBox(toLeftOf(row.cells[0])));
-    }
-});
-
-step("Clear all tasks", async function () {
-    await evaluate(() => localStorage.clear());
-});
-
-step("Open todo application", async function () {
-    await goto("todo.taiko.dev");
-});
-
-step("Must not have <table>", async function (table) {
-    for (var row of table.rows) {
-        assert.ok(!await text(row.cells[0]).exists(0, 0));
-    }
-});
-
+step("Add text to echo <echotext>",async function(echotext){
+    await focus(textBox('Enter a text to echo'));
+    await write(echotext, into(textBox("Enter a text to echo")))
+    await click(button("Echo"));
+}
+);
 step("Must display <message>", async function (message) {
-    assert.ok(await text(message).exists(0, 0));
+    assert.ok(await text(message).exists());
+});
+step("Clear echo items", async function()
+{
+    await click(button("Clear"));
 });
 
-step("Add tasks <table>", async function (table) {
-    for (var row of table.rows) {
-        await write(row.cells[0]);
-        await press('Enter');
-    }
-});
-
-step("Must have <table>", async function (table) {
-    for (var row of table.rows) {
-        assert.ok(await text(row.cells[0]).exists());
-    }
-});
